e1e220b10f11e167bd450231736a91aa8920ef40
StateInspector <83210618+StateInspector@users.noreply.github.com>
2022-02-23 14:34:56 +0400

    Username Visible on connecting and disconnecting

--- Chat/Server/Hubs/ChatHub.cs
+++ Chat/Server/Hubs/ChatHub.cs
@@ -3,11 +3,23 @@
 namespace Chat.Server.Hubs
 {
     public class ChatHub : Hub
-    {public override async Task OnConnectedAsync()
+    {
+        private static Dictionary<string, string> Users = new Dictionary<string, string>();
+        public override async Task OnConnectedAsync()
         {
-            await AddMessageToChat("", "User connected!");
+            string username = Context.GetHttpContext().Request.Query["username"];
+            Users.Add(Context.ConnectionId, username);
+            await AddMessageToChat(string.Empty, $"{username} connected!");
             await base.OnConnectedAsync();
         }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            string username = Users.FirstOrDefault(U => U.Key == Context.ConnectionId).Value;
+            await AddMessageToChat(String.Empty, $"{username} Left the chat"); 
+            Users.Remove(Context.ConnectionId);
+        }
+
         public async Task AddMessageToChat(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
