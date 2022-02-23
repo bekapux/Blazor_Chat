c45dac79e2da1de00b2fb544d4c48d1f8ae534be
StateInspector <83210618+StateInspector@users.noreply.github.com>
2022-02-23 14:07:49 +0400

    initial commit

---
+++ Chat/Server/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Chat.Server.Hubs
+{
+    public class ChatHub : Hub
+    {public override async Task OnConnectedAsync()
+        {
+            await AddMessageToChat("", "User connected!");
+            await base.OnConnectedAsync();
+        }
+        public async Task AddMessageToChat(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
